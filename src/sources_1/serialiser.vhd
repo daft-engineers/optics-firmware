3aff73245a2c6a12810f95e862777d341a043a97
MarkAHarley <markharley@hotmail.co.uk>
2023-05-02 13:04:19 +0100

    Tidy shift

--- src/sources_1/serialiser.vhd
+++ src/sources_1/serialiser.vhd
@@ -13,7 +13,7 @@ end parallel2serial;
 
 architecture Behavioral of parallel2serial is
 signal r_data_enable                  : std_logic;
-signal r_data                         : std_logic_vector(13 downto 0);
+signal r_data                         : unsigned(13 downto 0);
 
 begin
 o_data          <= r_data(0);
@@ -22,26 +22,10 @@ process(i_clk_slow,i_clk_fast)
 begin
 
 if rising_edge(i_clk_slow) then
-    r_data <= i_data;
+    r_data <= unsigned(i_data);
 elsif rising_edge(i_clk_fast) then
     
-    --for k in 0 to G_N-1 loop
-    --    r_data(k) <= r_data(k+1);
-    --end loop;
-    r_data(0) <= r_data(1);
-    r_data(1) <= r_data(2);
-    r_data(2) <= r_data(3);
-    r_data(3) <= r_data(4);
-    r_data(4) <= r_data(5);
-    r_data(5) <= r_data(6);
-    r_data(6) <= r_data(7);
-    r_data(7) <= r_data(8);
-    r_data(8) <= r_data(9);
-    r_data(9) <= r_data(10);
-    r_data(10) <= r_data(11);
-    r_data(11) <= r_data(12);
-    r_data(12) <= r_data(13);
-    r_data(13) <= '0';
+    r_data <= shift_right(r_data, 1);
     
 end if;
 end process;
